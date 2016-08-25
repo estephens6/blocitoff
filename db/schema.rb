f74366eb4c45d77cc6c5f25c6de9c765fc650f43
Eric Stephens <perkinsdrive@gmail.com>
2016-08-25 07:51:26 +0000

    minor edits / attempting to solve routing issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824041007) do
+ActiveRecord::Schema.define(version: 20160825042604) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
