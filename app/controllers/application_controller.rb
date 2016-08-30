180a9cae53382b6fe6c62b448664a91c057c33c0
Eric Stephens <perkinsdrive@gmail.com>
2016-08-30 09:21:07 +0000

    UI standardization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+#  before_filter :authenticate_user!
+#  before_action :authenticate_user!
+
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    welcome_index_url
+  end
   
-  # before_action :authenticate_user!
+    protected
+  def authenticate_user!(options={})
+    if user_signed_in?
+
+      super(options)
+    else
+      redirect_to welcome_index_url, :notice => 'log in please'
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,57 @@
 class UsersController < ApplicationController
+#  before_filter :authenticate_user!
+
+  
   def show
     @user = User.find(params[:id] || current_user.id)
 #    @items = @user.items
   end
+
+
+
+#end
+
+
+#before_filter :check_has_access  
+#before_action send_to "welcome_index" if @user == 'nil'
+
+  
+#  if current_user.id = nil
+#        redirect_to welcome_index
+#    else
+#    @user = User.find(params[:id] || current_user.id)
+#  end
+  
+  
+#  def show
+
+#    @user = User.find(params[:id] || current_user.id)
+#    @user = current_user.id ? current_user.id : nil
+
+
+#    @items = @user.items
+#  end
+  
+#  if @user == nil then redirect_to "welcome#index" end
+  
+  
+
+
+
+
+
+
+
+
+
+#
+#  protected
+#  def check_has_access
+#    if (User.find(params[:id]) || current_user.id) == nil
+#      redirect_to "/welcome/index"
+#    else
+#      redirect_to "/users/show"
+#    end
+#  end
+
 end
