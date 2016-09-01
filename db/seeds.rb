42c484155f217ec16d58416a1e299261a828d66c
Eric Stephens <perkinsdrive@gmail.com>
2016-09-01 08:20:51 +0000

    Seeding complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+15.times do
+    User.create!(
+#        username: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(8)
+        )
+end
+
+users = User.all
+
+40.times do
+    Item.create!(
+        user: users.sample,
+        name: Faker::Hacker.noun,
+        )
+end
+
+items = Item.all
+
+puts "Seeds have been planted!"
+puts "#{users.count} users have been grown!"
+puts "#{items.count} items are on lists!"
